f6c8262ded983dd373e24c56b205c3ce20161472
Clement Venard <cvenard@gmail.com>
2018-03-19 09:42:55 +0800

    Added comments for godocs

--- wechat_work.go
+++ wechat_work.go
@@ -1,3 +1,6 @@
+// Please check the github Readme for an example of how to use this package. This package will help make
+// sending notifications using WeChat Work 企业微信 easier. Make sure to refer to the api docs (chinese)
+// to be able to know how to correctly configer each struct before sending the notification.
 package wechat
 
 import (
@@ -14,6 +17,8 @@ const (
 	debugRespFormat = "Webhook %d\n  URL: %s\n  METHOD: %s\n  HEADERS: %s\n  REQUEST BODY: %s\n  RESPONSE STATUS: %s\n  RESPONSE BODY: %s\n"
 )
 
+// Config is the minimum information you need to send a notification, CorpId and CorpSecret are used to
+// get the access token required before sending a notification.
 type (
 	Config struct {
 		CorpID       string
@@ -23,6 +28,9 @@ type (
 		Notification Notification
 	}
 
+	// Notification defines information about who to send the notification to, the type of message and the agentid of your
+	// wechat work application. The other structs are to define which notification to send. If you define both a TextCard and a
+	// Text but set the message type to "text" then only a text will be sent.
 	Notification struct {
 		ToUser   string   `json:"touser"`
 		ToParty  string   `json:"toparty"`
@@ -39,6 +47,8 @@ type (
 		MpNews   MpNews   `json:"mpnews"`
 		Safe     int      `json:"safe"`
 	}
+
+	// TextCard is a text card that will be sent as a notification.
 	TextCard struct {
 		Title       string `json:"title"`
 		Description string `json:"description"`
@@ -46,32 +56,39 @@ type (
 		BtnTxt      string `json:"btntext"`
 	}
 
+	// Text is a text message
 	Text struct {
 		Content string `json:"content"`
 	}
 
+	// Image is  an image to send
 	Image struct {
 		MediaId string `json:"media_id"`
 	}
 
+	// Voice  sends a voice message
 	Voice struct {
 		MediaId string `json:"media_id"`
 	}
 
+	// Video sends a video with a title and description
 	Video struct {
 		MediaId     string `json:"media_id"`
 		Title       string `json:"title"`
 		Description string `json:"description"`
 	}
 
+	// File sends a file that needs to be uploaded to wechat work
 	File struct {
 		MediaId string `json:"media_id"`
 	}
 
+	// News is an array of articles to be sent
 	News struct {
 		Articles []Article `json:"articles"`
 	}
 
+	// Article defines an article to be sent
 	Article struct {
 		Title       string `json:"title"`
 		Description string `json:"description"`
@@ -106,7 +123,7 @@ type (
 	}
 )
 
-// When performing
+// gets the access token to be used in subsequent requests
 func getAccessToken(body []byte) (*Response, error) {
 	var s = new(Response)
 	err := json.Unmarshal(body, &s)
@@ -116,6 +133,7 @@ func getAccessToken(body []byte) (*Response, error) {
 	return s, err
 }
 
+// Exec on a completed WeChatWork struct sends a message
 func (p WeChatWork) Exec() error {
 
 	var b []byte
